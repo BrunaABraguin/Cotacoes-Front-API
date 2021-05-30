1f265342bf7dc8e23ff9a6ed6ff5043d2d315959
Bruna A Braguin <bruna.braguin@keyrus.com.br>
2021-05-30 03:35:07 -0300

    Montagem URL

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 import { StockCardComponent } from './stock-card/stock-card.component';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     DashboardPageComponent,
@@ -10,6 +10,7 @@ import { StockCardComponent } from './stock-card/stock-card.component';
   ],
   imports: [
     CommonModule,
+    HttpClientModule
   ],
 })
 export class DashboardModule { }
